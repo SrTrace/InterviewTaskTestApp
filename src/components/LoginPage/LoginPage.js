56fd575f9e225d8d2a447b9f7000e76e025b3271
Sergey <levashov04sergey@gmail.com>
2021-11-29 21:51:15 +0200

    made logic for input handlers

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import "./style.css";
 import {useDispatch, useSelector} from "react-redux";
+import {LOGIN, PASSWORD} from "../../constants/constants";
+import {login, password} from "../../redux/actions/actions";
 
 const LoginPage = () => {
     const state = useSelector(state=>  state.auth);
     const dispatch = useDispatch();
 
-    const handlerLogin = () => {
-
+    const handlerLogin = (event) => {
+        dispatch(login(LOGIN,event.target.value));
+        console.log(state.login);
     };
 
-    const handlerPassword = () => {
-
+    const handlerPassword = (event) => {
+        dispatch(password(PASSWORD,event.target.value));
+        console.log(state.password);
     };
 
     return (
         <form className="form">
-            <input className="form__input" type="text"/>
-            <input className="form__input" type="text"/>
+            <input
+                className="form__input" type="text"
+                onChange={handlerLogin}
+            />
+            <input
+                className="form__input" type="text"
+                onChange={handlerPassword}
+            />
             <button className="form__link">
                 Login
             </button>
