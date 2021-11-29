b64e4dae64668d48f30bb80f930c726905164086
Sergey <levashov04sergey@gmail.com>
2021-11-29 21:36:52 +0200

    add style for LoginPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import {useSelector} from "react-redux";
+import LoginPage from "./components/LoginPage/LoginPage";
+import React from "react";
 
 function App() {
     const state = useSelector(state=>  state.auth);
 
-    console.log(state);
+    //console.log(state);
   return (
     <div className="App">
-          Learn React
+          <h1>Enter</h1>
+        <LoginPage/>
     </div>
   );
 }
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import "./style.css";
+
 
 const LoginPage = () => {
     return (
         <form className="form">
             <input className="form__input" type="text"/>
             <input className="form__input" type="text"/>
-            <Link className="form__link">
+            <button className="form__link">
                 Login
-            </Link>
+            </button>
         </form>
     );
 };
