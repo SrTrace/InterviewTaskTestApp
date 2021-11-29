92ccfde0dbc0c4ee5e928c1c56135e8b52f86ee3
Sergey <levashov04sergey@gmail.com>
2021-11-29 21:43:00 +0200

    create func for action for changing state

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import "./style.css";
-
+import {useDispatch, useSelector} from "react-redux";
 
 const LoginPage = () => {
+    const state = useSelector(state=>  state.auth);
+    const dispatch = useDispatch();
+
+    const handlerLogin = () => {
+
+    };
+
+    const handlerPassword = () => {
+
+    };
+
     return (
         <form className="form">
             <input className="form__input" type="text"/>
